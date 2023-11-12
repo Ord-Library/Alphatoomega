9230585d23fa4b47e4de714f417e1bd3fad7544b
8bit <8bit@recursincribe.com>
2023-11-12 01:06:01 -0500

    Create css.js

---
+++ css.js
@@ -0,0 +1,29 @@
+body {
+    font-family: 'Satoshi', sans-serif;
+    background-color: #000;
+    color: #0f0;
+    text-align: center;
+}
+
+.container {
+    max-width: 600px;
+    margin: auto;
+    padding: 20px;
+}
+
+input, button {
+    padding: 10px;
+    margin: 5px;
+    border-radius: 5px;
+}
+
+#result {
+    margin-top: 20px;
+    font-size: 20px;
+}
+
+/* Matrix green glow background */
+body {
+    background: black;
+    background-image: radial-gradient(circle, #0f0, #000 60%);
+}
